5c0474f98aa3a68ab76949a1722e937b39810015
ARebel <accidentalrebel@gmail.com>
2020-04-02 19:17:03 +0800

    Can now close window with escape.

--- hello.cpp
+++ hello.cpp
@@ -7,6 +7,14 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 int main()
 {
   glfwInit();
@@ -35,6 +43,11 @@ int main()
 
 	while(!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
