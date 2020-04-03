2027eb0f82289ccd9febd7b977103c17eb5236c4
ARebel <accidentalrebel@gmail.com>
2020-04-03 23:16:31 +0800

    Interpolation.

--- hello.cpp
+++ hello.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <math.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
+	"layout (location = 0) in vec3 aPos;\n"
+	"layout (location = 1) in vec3 aColor;\n"
+	"out vec3 ourColor;\n"
+	"void main()\n"
+	"{\n"
+	"   gl_Position = vec4(aPos, 1.0);\n"
+	"   ourColor = aColor;\n"
+	"}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
 	"out vec4 FragColor;\n"
+	"in vec3 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"  FragColor = vec4(ourColor, 1.0);\n"
 	"}\0";
 
 float vertices[] = {
-	0.5f,  0.5f, 0.0f,  // top right
-	0.5f, -0.5f, 0.0f,  // bottom right
-	-0.5f, -0.5f, 0.0f,  // bottom left
-	-0.5f,  0.5f, 0.0f   // top left 
+	0.6f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right
+	0.5f, -0.6f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom right
+	-0.6f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // bottom left
+	-0.5f,  0.6f, 0.0f, 1.0f, 0.0f, 1.0f,   // top left 
 };
 unsigned int indices[] = {
 	0, 1, 3,
@@ -120,13 +125,18 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glEnableVertexAttribArray(1);
 	
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	// We unbind the VAO vertex array so it is not overwritten
 	glBindVertexArray(0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	
 	while(!glfwWindowShouldClose(window))
 	{
@@ -138,9 +148,13 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+ 		glUseProgram(shaderProgram);
 		
-		glUseProgram(shaderProgram);
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		
 		glBindVertexArray(VAO);
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
