b8c25d9b5193e6a9a6ca0d84f5909cfb22967960
ARebel <accidentalrebel@gmail.com>
2020-04-03 17:34:12 +0800

    Adjustments.

--- hello.cpp
+++ hello.cpp
@@ -98,11 +98,11 @@ int main()
 	// Configuration
 	// --------------------------------
 	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	
-	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
 	
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
