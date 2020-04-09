62460d67966802404213963a4c6937e60f35a236
ARebel <accidentalrebel@gmail.com>
2020-04-09 19:47:42 +0800

    Initial setup for a limited FPS game.

--- camera.h
+++ camera.h
@@ -63,14 +63,19 @@ class Camera
 	void ProcessKeyboard(Camera_Movement direction, float deltaTime)
 	{
 		float velocity = MovementSpeed * deltaTime;
-		if (direction == FORWARD)
+		float t = Position.y;
+					
+		if (direction == FORWARD) {
 			Position += Front * velocity;
+		}
 		if (direction == BACKWARD)
 			Position -= Front * velocity;
 		if (direction == LEFT)
 			Position -= Right * velocity;
 		if (direction == RIGHT)
 			Position += Right * velocity;
+
+		Position.y = t;
 	}
 
 	void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
--- hello.cpp
+++ hello.cpp
@@ -15,6 +15,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xOffset, double yOffset);
 void processInput(GLFWwindow *window);
+void displayMap(Shader *shader);
 
 Camera g_camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = 400.0f, lastY = 300.0f;
@@ -24,16 +25,16 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 glm::vec3 cubePositions[] = {
-  glm::vec3( 0.0f,  0.0f,  0.0f), 
-  glm::vec3( 2.0f,  5.0f, -15.0f), 
-  glm::vec3(-1.5f, -2.2f, -2.5f),  
-  glm::vec3(-3.8f, -2.0f, -12.3f),  
-  glm::vec3( 2.4f, -0.4f, -3.5f),  
-  glm::vec3(-1.7f,  3.0f, -7.5f),  
-  glm::vec3( 1.3f, -2.0f, -2.5f),  
-  glm::vec3( 1.5f,  2.0f, -2.5f), 
-  glm::vec3( 1.5f,  0.2f, -1.5f), 
-  glm::vec3(-1.3f,  1.0f, -1.5f)  
+  glm::vec3( 0.0f, 0,  0.0f), 
+  glm::vec3( 2.0f, 0, -15.0f), 
+  glm::vec3(-1.5f, 0, -2.5f),  
+  glm::vec3(-3.8f, 0, -12.3f),  
+  glm::vec3( 2.4f, 0, -3.5f),  
+  glm::vec3(-1.7f, 0, -7.5f),  
+  glm::vec3( 1.3f, 0, -2.5f),  
+  glm::vec3( 1.5f, 0, -2.5f), 
+  glm::vec3( 1.5f, 0, -1.5f), 
+  glm::vec3(-1.3f, 0, -1.5f)  
 };
 float vertices[] = {
     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
@@ -89,7 +90,6 @@ int main()
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
   GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
   if ( window == NULL )
@@ -232,20 +232,22 @@ int main()
 		glm::mat4 view = g_camera.GetViewMatrix();
 		int viewLoc = glGetUniformLocation(shader.ID, "view");
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		
-		for(unsigned int i = 0; i < 10 ; i++)
-		{
-			glm::mat4 model = glm::mat4(1.0f);
-			model = glm::translate(model, cubePositions[i]);
-			float angle = 20.0f * i;
-			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0));
-			// float scale = sin((float)i / 10 * glfwGetTime());
-			// model = glm::scale(model, glm::vec3(scale, scale, scale));
-			int modelLoc = glGetUniformLocation(shader.ID, "model");
-			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-			glDrawArrays(GL_TRIANGLES, 0, 36);
-		}
+		displayMap(&shader);
+		
+		// for(unsigned int i = 0; i < 10 ; i++)
+		// {
+		// 	glm::mat4 model = glm::mat4(1.0f);
+		// 	model = glm::translate(model, cubePositions[i]);
+		// 	float angle = 20.0f * i;
+		// 	model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0));
+		// 	// float scale = sin((float)i / 10 * glfwGetTime());
+		// 	// model = glm::scale(model, glm::vec3(scale, scale, scale));
+		// 	int modelLoc = glGetUniformLocation(shader.ID, "model");
+		// 	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+		// 	glDrawArrays(GL_TRIANGLES, 0, 36);
+		// }
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -261,6 +263,19 @@ int main()
 	
 	glfwTerminate();
   return 0;
+}
+
+void displayMap(Shader *shader)
+{
+		for(unsigned int i = 0; i < 10 ; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			int modelLoc = glGetUniformLocation(shader->ID, "model");
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
