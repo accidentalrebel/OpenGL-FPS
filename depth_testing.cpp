f17664a330f58a5a04c8c94a388219f12efa287b
ARebel <accidentalrebel@gmail.com>
2020-04-27 21:19:10 +0800

    Setup for Framebuffers lessons.

--- depth_testing.cpp
+++ depth_testing.cpp
@@ -199,6 +199,7 @@ int main()
 	unsigned int cubeTexture  = Shader::LoadTextureFromFile("marble.jpg", "assets/textures");
 	unsigned int floorTexture = Shader::LoadTextureFromFile("metal.png", "assets/textures");
 	unsigned int windowTexture = Shader::LoadTextureFromFile("blending_transparent_window.png", "assets/textures");
+	unsigned int containerTexture = Shader::LoadTextureFromFile("container.jpg", "assets/textures");
 
 	// shader configuration
 	// --------------------
@@ -207,6 +208,36 @@ int main()
 
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// Framebuffer setup
+	// -----------------
+	unsigned int frameBuffer;
+	glGenFramebuffers(1, &frameBuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
+
+	unsigned int texColorBuffer;
+	glGenTextures(1, &texColorBuffer);
+	glBindTexture(GL_TEXTURE_2D, texColorBuffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// Attach to bounded framebuffer object
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
+
+	unsigned int rbo;
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 800, 600);
+	glBindRenderbuffer(GL_RENDERBUFFER, 0);
+
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+	if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+	
+	glBindFramebuffer(GL_FRAMEBUFFER, 0); 
 
 	// render loop
 	// -----------
@@ -234,8 +265,8 @@ int main()
 		normalShader.setMat4("projection", projection);
 
 		drawFloor(normalShader, planeVAO, floorTexture);
-		drawTwoCubes(normalShader, cubeVAO, cubeTexture, 1.0f);
-		drawWindow(normalShader, windowVAO, windowTexture);
+		drawTwoCubes(normalShader, cubeVAO, containerTexture, 1.0f);
+		// drawWindow(normalShader, windowVAO, windowTexture);
 				
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
